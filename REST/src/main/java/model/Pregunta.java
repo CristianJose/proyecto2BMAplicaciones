43aeb713b0d91d41ca7043710bbad8c02cc0eda1
CristianJose <cristianmacas93@hotmail.com>
2019-01-11 04:49:34 -0500

    Finalizado app.js

--- REST/src/main/java/model/Pregunta.java
+++ REST/src/main/java/model/Pregunta.java
@@ -1,5 +1,6 @@
 package model;
 
+
 import javax.persistence.*;
 import java.io.Serializable;
 
@@ -10,21 +11,32 @@ public class Pregunta implements Serializable {
     @Column(name = "id_pregunta")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_pregunta;
+
     @Column(name = "nom_pregunta")
     private String nom_pregunta;
 
-    @ManyToOne
-    @JoinColumn(name="id_cuestionario")
-    private Cuestionario id_cuestionario;
+    @Column(name = "id_cuestionario")
+    private Long id_cuestionario;
+
+
 
     public Pregunta(Long id_pregunta, String nom_pregunta) {
         this.id_pregunta=id_pregunta;
         this.nom_pregunta = nom_pregunta;
+        this.id_cuestionario = id_cuestionario;
     }
 
     public Pregunta() {
     }
 
+    public  void consultarcuestionario (int codigo){
+        Cuestionario buscar = new Cuestionario();
+         int valor = buscar.getCod_cuestionario();
+        if (codigo == valor){
+
+        }
+
+    }
     public Long getId_pregunta() {
         return id_pregunta;
     }
@@ -41,4 +53,7 @@ public class Pregunta implements Serializable {
         this.nom_pregunta = nom_pregunta;
     }
 
-   }
+    public Long getId_cuestionario() { return id_cuestionario; }
+
+    public void setId_cuestionario(Long id_cuestionario) { this.id_cuestionario = id_cuestionario; }
+}
--- REST/src/test/java/TestData.java
+++ REST/src/test/java/TestData.java
@@ -7,26 +7,32 @@ public class TestData {
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         Transaction transaction = session.beginTransaction();
+        //Ingresar cuestionario
         Cuestionario cuestionario = new Cuestionario();
-        cuestionario.setCod_cuestionario(1122);
-        cuestionario.setNom_cuestionario("Cuestionario 6");
+        cuestionario.setCod_cuestionario(3333);
+        cuestionario.setNom_cuestionario("Cuestionario 19995");
         session.save(cuestionario);
 
-        Pregunta pregunta = new Pregunta();
-        pregunta.setNom_pregunta("Pregunta01");
-        session.save(pregunta);
+        //Ingresar pregunta
+        /*Pregunta pregunta = new Pregunta();
+        pregunta.setNom_pregunta("Pregunta funciona");
 
+        session.save(pregunta);*/
+/*
+        //ingresar respuesta
         Respuesta respuesta = new Respuesta();
         respuesta.setOpcion_respuesta("Respuesta01");
         respuesta.setVerificacion(1);
         session.save(respuesta);
-        /*
+        *//*
+        //Ingresar resultado
         Resultado resultado = new Resultado();
         resultado.setId_prueba(9);
         resultado.setId_pregunta(5);
         resultado.setValor(100);
         session.save(resultado);
 
+        //Resultado corta
         Resultado_corta resultado_corta = new Resultado_corta();
         resultado_corta.setId_prueba(9);
         resultado_corta.setId_pregunta(5);
